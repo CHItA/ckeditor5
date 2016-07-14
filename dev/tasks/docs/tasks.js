cf1d2666919a4f1632b39d8e903dfa6fe00950ad
Kamil Piechaczek <pomek@pomek.pl>
2016-07-14 16:16:30 +0200

    Introduced gulp task which builds documentation using Docs-Builder.

--- dev/tasks/docs/tasks.js
+++ dev/tasks/docs/tasks.js
@@ -6,37 +6,48 @@
 'use strict';
 
 const gulp = require( 'gulp' );
-const jsdoc = require( 'gulp-jsdoc3' );
 const path = require( 'path' );
+const fs = require( 'fs-extra' );
 const collectFiles = require( './tasks/collect-files' );
+const docsBuilder = require( 'docs-builder' );
+const runSequence = require( 'run-sequence' );
 
 module.exports = ( config ) => {
 	const tasks = {
 		buildDocs( cb ) {
-			const esnextBuildPath = path.join( config.ROOT_DIR, config.BUILD_DIR, 'esnext' );
-			const jsDocConfig = {
-				opts: {
-					encoding: 'utf8',
-					destination: path.join( config.BUILD_DIR, 'docs' ),
-					recurse: true,
-					access: 'all'
-				},
-				plugins: [
-					'node_modules/jsdoc/plugins/markdown',
-					'dev/tasks/docs/plugins/comment-fixer'
-				]
+			// Absolute path to root directory of project.
+			const projectRoot = path.join( __dirname, '..', '..', '..' );
+			// Absolute path to "esnext" build of project.
+			const esnextBuildPath = path.join( projectRoot, config.BUILD_DIR, 'esnext' );
+			// Absolute path to documentation sources.
+			const absoluteDocsSourcePath = path.join( projectRoot, config.DOCUMENTATION_SOURCE_DIR );
+
+			const builderConfig = {
+				name: 'CKEditor5 Documentation',
+				disqusUrl: 'demo-test-pomek',
+				rootDirectory: absoluteDocsSourcePath + path.sep,
+				destinationDirectory: path.join( projectRoot, config.DOCUMENTATION_DIR ),
+				guideFiles: path.join( 'guides', '**', '*.md' ),
+				sampleFiles: path.join( 'samples', '**', '*.md' ),
+				indexFile: path.join( projectRoot, 'docs', 'index.pug' ),
+				jsDocFiles: [
+					path.join( projectRoot, 'README.md' ),
+					path.join( esnextBuildPath, '**', '*.@(js|jsdoc)' ),
+					'!' + path.join( esnextBuildPath, 'ckeditor5', '*', 'lib', '**', '*' )
+				],
+				cdnFiles: {
+					js: [
+						'https://rawgit.com/pomek/5def0512e6096893c1ca771d26d0b488/raw/9cc7be7a0c59f7e0bd092bcce95ae688813151a7/ckeditor.js'
+					],
+					css: [
+						'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+						'https://rawgit.com/pomek/0bc4cb6a2b1bcdadbf69fec0dbf7cde7/raw/439c96e8a932f79e13e9080b45af0fa212da7e00/ckeditor.css'
+					]
+				}
 			};
 
-			const patterns = [
-				'README.md',
-				// Add all js and jsdoc files, including tests (which can contain utils).
-				path.join( esnextBuildPath, '**', '*.@(js|jsdoc)' ),
-				// Filter out libs.
-				'!' + path.join( esnextBuildPath, 'ckeditor5', '*', 'lib', '**', '*' )
-			];
-
-			gulp.src( patterns, { read: false } )
-				.pipe( jsdoc( jsDocConfig, cb ) );
+			docsBuilder( builderConfig )
+				.then( cb );
 		},
 
 		collectGuideFiles() {
@@ -47,10 +58,24 @@ module.exports = ( config ) => {
 			return collectFiles( config, 'samples', [ 'md', 'html', 'js' ] );
 		},
 
+		removeCollectedFiles() {
+			fs.removeSync( config.DOCUMENTATION_SOURCE_DIR );
+		},
+
 		register() {
-			gulp.task( 'docs', [ 'build:js:esnext' ], tasks.buildDocs );
-			gulp.task( 'docs:prepare:guides', tasks.collectGuideFiles );
-			gulp.task( 'docs:prepare:samples', tasks.collectSampleFiles );
+			gulp.task( 'docs', ( cb ) => {
+				runSequence( 'docs:clean', 'docs:prepare', 'docs:build', cb );
+			} );
+
+			gulp.task( 'docs:build', tasks.buildDocs );
+			gulp.task( 'docs:clean', tasks.removeCollectedFiles );
+			gulp.task( 'docs:collect:guides', tasks.collectGuideFiles );
+			gulp.task( 'docs:collect:samples', tasks.collectSampleFiles );
+			gulp.task( 'docs:prepare', [
+				'docs:collect:guides',
+				'docs:collect:samples',
+				'build:js:esnext'
+			] );
 		}
 	};
 
